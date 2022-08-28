4d0c32f52b21531bf8e7def4bcbf5f39fa488f5e
Osaro Onaiwu <Osernon@gmail.com>
2022-08-28 22:45:06 +0000

    4th

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-// import Hamburger from './Hamburger';
-// import NavBar from './NavBar';
-// import { useGlobalContext } from '../context';
-
-// const Header = () => {
-//   const { darkMode } = useGlobalContext();
-
-//   return (
-//     <section className='header'>
-//       <a href='https://osarodev.com'>
-//         {' '}
-//         <img
-//           className='logo'
-//           src={`${darkMode ? darklogo : lightlogo}`}
-//           alt='osarodev logo'
-//         />
-//       </a>
-//       <div>
-//         <Hamburger />
-//         <NavBar />
-//       </div>
-//     </section>
-//   );
-// };
-
 
 const Header = () => {
     return (
